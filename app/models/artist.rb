05ec26e837d51d2587b0e27ee71b440c5ec349f8
Rose Maina <rosewairimu27@gmail.com>
2022-08-25 00:14:16 +0300

    Done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,23 @@
 # add the Artist class here
+
+#connect to a database
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/artists.sqlite"
+  )
+#The SQL code for creating the table
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+#create an Artist class and extend the class with ActiveRecord::Base:
+class Artist < ActiveRecord::Base
+end
